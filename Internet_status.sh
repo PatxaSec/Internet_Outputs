6d08011fee5788adebe63bab4bb52c5d92cf0386
Patxa <106649207+Patxa-py@users.noreply.github.com>
2023-09-04 09:16:38 +0200

    Update Internet_status.sh

--- Internet_status.sh
+++ Internet_status.sh
@@ -4,6 +4,8 @@
 
 if [ -n "$(ifconfig eth0 2>/dev/null | grep 'inet ')"]; then
     echo "$(/usr/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2}')"
+elif [ -n "$(ifconfig ens33 2>/dev/null | grep 'inet ')"]; then
+    echo "$(/usr/sbin/ifconfig ens33 | grep 'inet ' | awk '{print $2}')"
 elif [ -n "$(ifconfig wlan0 2>/dev/null | grep 'inet ')"]; then
     echo "$(/usr/sbin/ifconfig wlan0 | grep 'inet ' | awk '{print $2}')"
 else
